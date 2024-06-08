510ffc42113e80fa6cbcf5f7c3314c9cddc0c5a9
alex <84686499+Dr-Ice1@users.noreply.github.com>
2024-06-08 18:12:42 +0200

    Add files via upload

---
+++ options.lua
@@ -0,0 +1,19 @@
+local opt = vim.opt
+
+-- Tab / Indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.autoindent = true
+
+-- Appearance
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
+
+opt.mouse:append("a")
+opt.clipboard:append("unnamedplus")
+
+
